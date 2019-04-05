bf277e003681c427a6b0d4d100d5a3efa6b1f185
RD <renedahlhansen@gmail.com>
2019-04-05 14:15:09 +0200

    Adding files part 1

---
+++ AarhusWebDevCoop/ViewModels/ContactForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AarhusWebDevCoop.ViewModels
+{
+    public class ContactForm
+    {
+        [Required(ErrorMessage ="Please enter your name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your mail")]
+        [Display(Name ="Email")]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Please enter a correct e-mail adress")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter a subject")]
+        public string Subject { get; set; }
+        [Required(ErrorMessage = "Please enter a message")]
+        public string Message { get; set; }
+    }
+
+
+}
